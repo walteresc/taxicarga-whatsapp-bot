778a7999598225edf8870ad55463021526ad83ef
Walteresc <wescobarag@gmail.com>
2026-09-16 18:09:31 -0500

    Quita el subtítulo redundante del paso "Servicio y ruta"

--- frontend_materio/src/pages/cotizar.vue
+++ frontend_materio/src/pages/cotizar.vue
@@ -182,7 +182,6 @@ const submitSignup = async () => {
           <VWindow v-model="step">
             <VWindowItem :value="1">
               <div class="text-h6 font-weight-bold mb-1">Elige el tipo de servicio que mejor se adapta a tu carga</div>
-              <p class="text-caption text-medium-emphasis mb-3">Selecciona el tipo de servicio que mejor se adapte a tu necesidad.</p>
               <VRow class="mb-3" dense>
                 <VCol v-for="s in SERVICE_TYPES" :key="s.value" cols="12" sm="4">
                   <VCard
